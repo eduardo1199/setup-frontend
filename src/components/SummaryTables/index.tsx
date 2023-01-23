7f2fe9b8c966acffca22ef83757f550f513241c5
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-01-23 20:29:39 -0300

    first commit project setup nlw

---
+++ src/components/SummaryTables/index.tsx
@@ -0,0 +1,39 @@
+import { generateRangeFromYearBeginning } from "../../utils/generate-range-from-year-beginnig";
+import { HabitDay } from "./HabitDay";
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+const summaryDates = generateRangeFromYearBeginning();
+
+const minimumSummaryDatesSizes = 18 * 7; // 18 semanas
+const amountOfDatesToFill = minimumSummaryDatesSizes - summaryDates.length;
+
+export function SummaryTables() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+       {weekDays.map((weekDay, index) => {
+        return (
+            <div key={`${weekDay}-${index}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
+              {weekDay}
+            </div>
+          )
+       })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((summaryDate, index) => {
+          return (
+            <HabitDay key={summaryDate.toString()} />
+          )
+        })}
+
+        {amountOfDatesToFill > 0 && Array.from({ length: amountOfDatesToFill }).map((_, index) => {
+          return (
+            <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
