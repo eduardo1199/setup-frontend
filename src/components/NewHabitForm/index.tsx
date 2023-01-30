cf4b9012e491b4d3d944fa94b8989e11fa0e47c7
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-01-30 16:49:13 -0300

    craate checkbox in modal form create habit

--- src/components/NewHabitForm/index.tsx
+++ src/components/NewHabitForm/index.tsx
@@ -1,8 +1,43 @@
 import { Check } from "phosphor-react";
 
+import { FormEvent, useState } from 'react';
+
+import * as CheckBox from '@radix-ui/react-checkbox';
+
+const availableWeekDays = [
+  'Domingo',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado-feira',
+
+]
+
 export function NewHabitForm() {
+  const [title, setTitle] = useState('');
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function createNewHabitForm(event: FormEvent) {
+    event.preventDefault();
+
+    
+  }
+
+  function handleToggleWeekDays(weekDay: number) {
+    if(weekDays.includes(weekDay)) {
+      const weekDaysWithRemovedOne = weekDays.filter((day) => day === weekDay)
+
+      setWeekDays(weekDaysWithRemovedOne);
+    } else {
+      const weekDaysWithAddedOne = [...weekDays, weekDay]
+
+      setWeekDays(weekDaysWithAddedOne)
+    }
+  }
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={createNewHabitForm} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprometimento?
       </label>
@@ -13,11 +48,35 @@ export function NewHabitForm() {
         placeholder="ex.:Exercícios, dormir bem, ect... "
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
+        onChange={event => setTitle(event.target.value)}
+        value={title}
       />
 
       <label htmlFor="title" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+       {availableWeekDays.map((availableWeek, index) => {
+        return (
+            <CheckBox.Root
+              className="flex items-center gap-3 group"
+              key={availableWeek}
+              onCheckedChange={() => handleToggleWeekDays(Number(availableWeek))}
+            >
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <CheckBox.Indicator>      
+                <Check size={20} className="text-white"/>
+              </CheckBox.Indicator>
+            </div>
+
+            <span className="text-white leading-tight">
+              {availableWeek}
+            </span>
+          </CheckBox.Root>
+        )
+       })}
+      </div>
 
       <button 
         type="submit" 
--- src/components/SummaryTables/HabitDay/index.tsx
+++ src/components/SummaryTables/HabitDay/index.tsx
@@ -1,7 +1,10 @@
 import * as Popover from '@radix-ui/react-popover';
 import { ProgressBar } from '../ProgressBar';
 
+import * as CheckBox from '@radix-ui/react-checkbox';
+
 import clsx from 'clsx';
+import { Check } from 'phosphor-react';
 
 interface HabitDay {
   completed: number;
@@ -31,6 +34,22 @@ export function HabitDay(props: HabitDay){
           <span className="mt-1 font-extrabold leading-tight text-3xl">26/01</span>
 
           <ProgressBar progress={10} /> 
+
+          <div className="mt-6 flex flex-col gap-3">
+            <CheckBox.Root
+              className="flex items-center gap-3 group"
+            >
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <CheckBox.Indicator>      
+                  <Check size={20} className="text-white"/>
+                </CheckBox.Indicator>
+              </div>
+
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-500">
+                Beber 2L de água
+              </span>
+            </CheckBox.Root>
+          </div>
 
           <Popover.Arrow className="fill-zinc-900" height={8} width={16} />
         </Popover.Content>
